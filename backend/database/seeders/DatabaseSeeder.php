f024a6de41486deda946e2d5caef41a3696a22e2
tiardiz <ardaktleules11@gmail.com>
2025-07-19 15:52:26 +0500

    del *.sqilte from .gitignore, readded bd, edit puth in .env.example, add correct instruction

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -9,17 +9,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    
         User::firstOrCreate(
         ['email' => 'test@example.com'],
         [
